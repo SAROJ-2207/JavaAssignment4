16447a2acb88350f5798c96265451b050392c343
SAROJ-2207 <sksaroj828@gmail.com>
2022-04-12 00:56:54 +0530

    Added:The Player rolls the die to get a number between 1 to 6.

---
+++ Ladder.java
@@ -0,0 +1,12 @@
+
+public class Ladder {
+
+	int initialPosition;
+	int finalPosition;
+	
+	public Ladder( int initialPosition, int finalPosition )
+	{
+		this.initialPosition = initialPosition;
+		this.finalPosition = finalPosition;
+	}
+}
