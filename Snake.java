40e5c1f4edd8de9a76edf63cd685fc2d39e02a8a
SAROJ-2207 <sksaroj828@gmail.com>
2022-04-12 01:03:35 +0530

    Added:Play the game with 2 Player. In this case if a Player gets a Ladder then plays again

---
+++ Snake.java
@@ -0,0 +1,11 @@
+public class Snake {
+	
+	int initialPosition;
+	int finalPosition;
+	
+	public Snake( int initialPosition, int finalPosition )
+	{
+		this.initialPosition = initialPosition;
+		this.finalPosition = finalPosition;
+	}
+}
