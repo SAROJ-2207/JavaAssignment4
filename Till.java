caec6317a1caf20a32ee4d37042af368fbea3a36
SAROJ-2207 <sksaroj828@gmail.com>
2022-04-12 01:06:45 +0530

    Repeat till the Player reaches the winning position 100

---
+++ Till.java
@@ -0,0 +1,15 @@
+public class Tile {
+	
+	int value;
+	Snake snake;
+	Ladder ladder;
+	
+	public Tile( int value, Snake snake, Ladder ladder )
+	{
+		this.value = value;
+		this.snake = snake;
+		this.ladder = ladder;
+	}
+	
+	
+}
